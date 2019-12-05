71d6d766079bceab484d5913b4fe9c8c6c8fc178
Dlin2744 <w4559731234@gmail.com>
2019-12-05 16:41:32 +0800

    Initial Commit

---
+++ 00657131_app3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00657131_app3
+//
+//  Created by User07 on 2019/12/5.
+//  Copyright © 2019 User07. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
